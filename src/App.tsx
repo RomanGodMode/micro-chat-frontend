cb8e9c98ef75c83decf60aee5bba5f5420eef8cd
RomanGodMode <roma.doter@yandex.ru>
2021-04-21 21:52:38 +0300

    proved websocket-gateway true of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import io from "socket.io-client"
+
+export type Message = {
+  text: string
+}
+
+const socket = io("http://localhost:8080")
 
 function App() {
+  const [messages, setMessages] = useState<Message[]>([])
+  useEffect(() => {
+    socket.on('message', ({text} : Message) => {
+      console.log(text)
+    })
+    socket.emit('message', {text: 'Игорь!?'})
+  }, [])
+
   return (
     <div className="App">
 
